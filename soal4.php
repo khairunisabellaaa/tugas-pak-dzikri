b6bf1c9b4b6849e16fe28cb9efdf531abae2bcbc
bella <111962970+khairunisabellaaa@users.noreply.github.com>
2023-10-23 14:05:20 +0700

    Add files via upload

---
+++ soal4.php
@@ -0,0 +1,9 @@
+<?php
+
+for ($i = 19; $i >= 1; $i -= 2) {
+    echo $i . " - aku seorang web developer\n <br>";
+}
+
+echo "<br><br>";
+
+?>
---
+++ soal5.php
@@ -0,0 +1,11 @@
+<?php
+
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 2 != 0) {
+        echo $i . ". Hoby aku coding\n <br>";
+    } else {
+        echo $i . ". Aku sangat senang koding\n <br>";
+    }
+}
+
+?>
